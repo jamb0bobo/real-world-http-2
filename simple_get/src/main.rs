a108511cdb211c0048da4dd8d46891c0e89ff347
jamb0bobo <100462573+jamb0bobo@users.noreply.github.com>
2022-03-04 00:44:12 +0000

    implement get method with surf, async-std and serd_json

--- simple_get/src/main.rs
+++ simple_get/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, simple_get");
+// use serde::{Deserialize, Serialize};
+use serde_json::{Value};
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut res = surf::get("https://httpbin.org/get").await?;
+    let body = res.body_string().await?;
+    dbg!(&body);
+    let v: Value = serde_json::from_str(&body)?;
+    println!("{}", v);
+    dbg!(v);
+
+    // #[derive(Deserialize, Serialize)]
+    // struct Ip {
+    //     ip: String
+    // }
+    // let mut res = surf::get("https://api.ipify.org?format=json").await?;
+    // let Ip { ip } = res.body_json().await?;
+    // dbg!(ip);
+    Ok(())
 }

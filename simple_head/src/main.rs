c87e35c6326c841d4773ba7249cdebdf57a05d7d
jamb0bobo <100462573+jamb0bobo@users.noreply.github.com>
2022-03-05 01:43:16 +0000

    implement simple_head

--- simple_head/src/main.rs
+++ simple_head/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // https://github.com/http-rs/surf/issues/218#issuecomment-722028760
+    let res = surf::head("https://httpbin.org/get").await?;
+    dbg!(res.status());
+    for name in res.header_names() {
+        let s = format!("{}: {}",name, res.header(name).unwrap());
+        dbg!(s);
+    };
+    Ok(())
 }
